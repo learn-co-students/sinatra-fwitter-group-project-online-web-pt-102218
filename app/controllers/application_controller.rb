16719417b61fcd6d7c91dc5275ed61ce5556911b
Johnathon Wood <jaywood128@gmail.com>
2019-02-24 23:06:31 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
   end
 
   get '/' do
@@ -15,6 +17,9 @@ class ApplicationController < Sinatra::Base
   helpers do
     def logged_in?
       !!session[:user_id]
+    end
+    def current_user
+      User.find(session[:user_id])
     end
 end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,10 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
-
+    if !logged_in?
+      redirect to '/login'
+    else
   erb :'tweets/tweets'
+    end
 end
 end
