7d4f32fd0c5b648a9ce1406b1153ab7224fab043
Brandon Teich <brandonteich123@gmail.com>
2019-03-20 00:18:22 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,35 @@
 require './config/environment'
+require 'sinatra/base'
+
 
 class ApplicationController < Sinatra::Base
-
   configure do
+    set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
   
-  #INDEX PAGE
   get '/' do
     erb :index
   end
-  
-  #SIGN UP PAGE
-  get '/signup' do
-    if Helpers.is_logged_in?(session)
-      redirect to '/tweets'
-  end
-end
 
+  def logged_in?
+      !!current_user
+  end
+  
+  def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+  
+  def redirect_if_not_logged_in
+    if !logged_in?
+      redirect '/'
+    end
+  end
+
+  def authorized_to_edit?(a_tweet)
+    a_tweet.user == current_user
+  end
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,21 @@
-#USER MODEL
-class User < ActiveRecord::Base
-  
-  #HELPER FUNCTIONS GO HERE
 
-  def slug()
-    username = self.username.downcase
-    username_array = username.split(/\W/).delete_if do
-      |word| word == "" 
-    end
-    slug_user = username_array.join("-")
-    slug_user
+class User < ActiveRecord::Base
+  has_many :tweets
+
+  has_secure_password
+
+  def slugy
+     slug = self.username.gsub(" ", "-").downcase
   end
 
   def self.find_by_slug(slug)
-    User.all.find do |u| 
-     u.slug == slug 
-    end
+    all.map 
+    {
+      |x|
+      if x.slug == slug
+            return x
+       end
+    }
   end
-  
+
 end
